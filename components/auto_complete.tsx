89d54acdc0282ae3d5a4c7633614493a6c405294
manki <cpro95@gmail.com>
2023-03-29 13:44:00 +0900

    search input box finished

--- components/auto_complete.tsx
+++ components/auto_complete.tsx
@@ -1,3 +1,4 @@
+import { useRouter } from "next/router";
 import React, { useRef, useState } from "react";
 
 type Props = {
@@ -10,20 +11,29 @@ function AutoComplete(props: Props) {
   const { items, value, onChange } = props;
   const ref = useRef<HTMLDivElement>(null);
   const [open, setOpen] = useState(false);
+  const router = useRouter();
 
   return (
     <div
       className={open ? "dropdown dropdown-open w-full" : "dropdown w-full"}
       ref={ref}
     >
-      <input
-        type="text"
-        className="input input-bordered w-full"
-        value={value}
-        onChange={(e) => onChange(e.target.value)}
-        placeholder="Type Address..."
-        tabIndex={0}
-      />
+      <div className="flex">
+        <input
+          type="text"
+          className="input input-bordered w-full"
+          value={value}
+          onChange={(e) => onChange(e.target.value)}
+          placeholder="Type Address..."
+          tabIndex={0}
+        />
+        <button
+          className="btn btn-outline ml-2"
+          onClick={() => router.push(value)}
+        >
+          Go
+        </button>
+      </div>
 
       <div className="dropdown-content bg-base-200 top-14 max-h-96 overflow-auto flex-col rounded-md">
         <ul
